d5af7cc358288818a2c3953dd675a51a04ecb892
Armani Raleigh <armanijolee@gmail.com>
2016-09-23 09:50:19 -0400

    UserInput for first Step

--- src/lab8.java
+++ src/lab8.java
@@ -1,12 +1,17 @@
 import java.util.Arrays;
+import java.util.Scanner;
 public class lab8 {
 
 	public static void main(String[] args) {
 		//declare statements
+		Scanner input = new Scanner(System.in);
+		int [][] batAverage;
+		int userInput;
+		
 //enter welcome screen
 		System.out.println( "Welcome to Battling Average Calculator!");
 		System.out.println( "Enter number of times at bat: ");
-		
+		userInput = input.nextInt();
 		
 		
 	}//End of main
