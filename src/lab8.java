ee6fe5d70a7b1a69d80ebd221f02cf3b54cf01d1
Armani Raleigh <armanijolee@gmail.com>
2016-09-23 09:41:54 -0400

    Beginning of Lab 8

---
+++ src/lab8.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class lab8 {
+
+	public static void main(String[] args) {
+		//declare statements
+//enter welcome screen
+		System.out.println( "Welcome to Battling Average Calculator!");
+		System.out.println( "Enter number of times at bat: ");
+		
+		
+		
+	}//End of main
+
+}//End of class
